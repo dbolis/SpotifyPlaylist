a3507b53c1fbb95d31d2d1ceede4a9ac22b2598e
Daniel Bolis <daniel@Daniels-MacBook-Pro-2.local>
2021-04-17 21:54:25 -0400

    added crate playlist funcationality

--- main.py
+++ main.py
@@ -11,6 +11,7 @@ class SaveSongs:
         self.DJ_playlist = DJ_playlist
         self.tracks = ""
         self.features=""
+        self.new_playlist_id=""
 
     def find_songs(self):
         print("Finding Songs")
@@ -28,6 +29,7 @@ class SaveSongs:
         
         print(self.tracks)  
         
+        self.get_song_features()
 
     def get_song_features(self):
         
@@ -40,6 +42,8 @@ class SaveSongs:
 
         self.features = response.json()["audio_features"]
         print(self.features)
+
+        self.filter('n','n','n','n','n','n')
 
 
     def filter(self, dance, energy, acoustic, instrumental, valence, tempo):
@@ -79,17 +83,51 @@ class SaveSongs:
             print("tempo: " + str(track["tempo"]))
             print("--------")
 
-        out = []
+        out = ""
         for track in self.features:
             if ranges[0][0]<=track["danceability"]<=ranges[0][1] and ranges[1][0]<=track["energy"]<=ranges[1][1] and \
             ranges[2][0]<=track["acousticness"]<=ranges[2][1] and ranges[3][0]<=track["instrumentalness"]<=ranges[3][1] and \
             ranges[4][0]<=track["valence"]<=ranges[4][1] and ranges[5][0]<=track["tempo"]<=ranges[5][1]:
 
-                out.append(track["id"])
+                out += track["uri"] + ","
 
+        out = out[:-1]
         print (out)
+        self.tracks=out
 
-                
+        self.add_to_playlist()
+
+    def create_playlist(self):
+        print("creating playlist")
+        query = "https://api.spotify.com/v1/users/{}/playlists".format(spotify_user_id)
+
+        request_body = json.dumps({
+            "name": "playlist name",
+            "description": "test playlist",
+            "public": True
+        })
+
+        response = requests.post(query, data=request_body,  headers={
+            "Content-Type": "application/json", 
+            "Authorization": "Bearer {}".format(self.spotify_token)
+        })
+            
+        response_json = response.json()
+        return response_json["id"]
+
+    def add_to_playlist(self):
+
+        self.new_playlist_id = self.create_playlist()
+        print(self.tracks)
+        print(self.new_playlist_id)
+        query ="https://api.spotify.com/v1/playlists/{}/tracks?uris={}".format(self.new_playlist_id, self.tracks)
+
+        response = requests.post(query,  headers={
+            "Content-Type": "application/json", 
+            "Authorization": "Bearer {}".format(self.spotify_token)
+        })
+    
+        print(response.json)
 
     def call_refresh(self):
         refreshCaller = Refresh()
@@ -103,6 +141,5 @@ class SaveSongs:
 a=SaveSongs()
 a.call_refresh()
 a.find_songs()
-a.get_song_features()
-a.filter('n','n','n','n','n','n')
+
 
